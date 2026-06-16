5cb09f08d609fffe10f6f3123520082357f66df7
Luis Chamberlain <mcgrof@do-not-panic.com>
2026-06-16 12:23:54 -0700

    [Core][Storage] Re-export L1MemoryUsageProvider from memory_manager pkg
    The slab-pool feature added L1MemoryUsageProvider in l1_memory_manager,
    but the memory_manager package __init__ does not re-export it. Callers and tests import it as
    `from lmcache.v1.distributed.memory_manager import L1MemoryUsageProvider`
    (the pre-refactor API), so re-export it from the package __init__.
    
    Signed-off-by: Luis Chamberlain <mcgrof@do-not-panic.com>

--- lmcache/v1/distributed/memory_manager/__init__.py
+++ lmcache/v1/distributed/memory_manager/__init__.py
@@ -18,6 +18,7 @@ from lmcache.v1.distributed.memory_manager.gds_l1_memory_manager import (
 from lmcache.v1.distributed.memory_manager.l1_manager_protocol import L1ManagerProtocol
 from lmcache.v1.distributed.memory_manager.l1_memory_manager import (
     L1MemoryManager,
+    L1MemoryUsageProvider,
     create_memory_allocator,
 )
 
@@ -26,5 +27,6 @@ __all__ = [
     "GDSL1MemoryManager",
     "L1ManagerProtocol",
     "L1MemoryManager",
+    "L1MemoryUsageProvider",
     "create_memory_allocator",
 ]
